7415dc5db3365964e5623c7b115f7278ac0ecdc1
Abhay Vishwakarma <gaamaofficial@gmail.com>
2024-09-08 07:15:37 +0530

    nav updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,10 +6,10 @@ function Nav() {
     <div className='w-full flex items-center justify-between font-medium py-5'>
         <div className="logo">Gaama.</div>
         <ul className='hidden sm:flex gap-5 text-sm'>
-            <NavLink to={`/`} className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before`}>Home</NavLink>
-            <NavLink to={`/about`}  className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before`}>about</NavLink>
-            <NavLink to={`/collection`} className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before`}>collection</NavLink>
-            <NavLink to={`/contact`} className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before`}>contact</NavLink>
+            <NavLink to={`/`} className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before px-1`}>Home</NavLink>
+            <NavLink to={`/about`}  className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before px-1`}>about</NavLink>
+            <NavLink to={`/collection`} className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before px-1`}>collection</NavLink>
+            <NavLink to={`/contact`} className={({isActive}) =>` ${isActive ? 'if-css-active' : ''} relative hvr-css-before px-1`}>contact</NavLink>
         </ul>
     </div>
   )
